211838fed6867cd5d19c4e270f89498557e864b0
David Zelaya <dzelayasegura@gmail.com>
2026-02-22 00:56:30 -0600

    python intermedio
    Ejercicios/bubble sort

---
+++ Semana 5/bubble sort 2.py
@@ -0,0 +1,14 @@
+def bubble_sort(arr):
+    n=len(arr)
+    for i in range(n):
+        swap=False
+        for d in range(0,n-i-1):
+            if arr[d]<arr[d+1]:
+                arr[d],arr[d+1]=arr[d+1],arr[d]
+                swap=True
+        if not swap:
+            break
+    return arr
+
+numbers=[5,3,8,4]
+print(bubble_sort(numbers))
