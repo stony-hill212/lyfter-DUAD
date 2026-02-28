38579127fd1e73d05bda52fc44d77bbbcb129b1a
David Zelaya <dzelayasegura@gmail.com>
2026-02-27 21:09:08 -0600

    Update Big O 2.py
    correcciones

--- Semana 6/Big O 2.py
+++ Semana 6/Big O 2.py
@@ -5,11 +5,11 @@ def print_numbers_times_2(numbers_list): #O(1)
 		
 
 #2
-def check_if_lists_have_an_equal(list_a, list_b): #O(1)
+def check_if_lists_have_an_equal(list_a, list_b): #O(nm)/O(n^2) peor caso
 	for element_a in list_a: #O(n)
-		for element_b in list_b: #O(m)
-			if element_a == element_b: #O(1)
-				return True #O(1)
+		for element_b in list_b: #O(nm)/O(n^2)
+			if element_a == element_b: #O(nm)/O(n^2)
+				return True #O(1) mejor caso
 				
 	return False #O(1)
 
@@ -21,11 +21,11 @@ def print_10_or_less_elements(list_to_print): #O(1)
 		
 
 #4
-def generate_list_trios(list_a, list_b, list_c): #O(1)
+def generate_list_trios(list_a, list_b, list_c): #O(nmk)/O(n^3)
 	result_list = [] #O(1)
 	for element_a in list_a: #O(n)
-		for element_b in list_b: #O(m)
-			for element_c in list_c: #O(k)
-				result_list.append(f'{element_a} {element_b} {element_c}')  #O(1)
+		for element_b in list_b: #O(nm)/O(n^2)
+			for element_c in list_c: #O(nmk)/O(n^3)
+				result_list.append(f'{element_a} {element_b} {element_c}')  #O(nmk)/O(n^3)
 				
 	return result_list  #O(1)
