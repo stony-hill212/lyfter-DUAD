6a9137928ac74c3fcea17a420143e0ed15b762db
David Zelaya <dzelayasegura@gmail.com>
2026-03-12 21:20:54 -0600

    Update persistence.py

--- Semana 8/personal_finance_tracker/persistence.py
+++ Semana 8/personal_finance_tracker/persistence.py
@@ -1,8 +1,27 @@
 import csv
+import os
 from datetime import datetime
 
 file_name="finance_data.csv"
 date_format="%d/%m/%Y"
+
+def append_to_csv(movement,target=file_name):
+    with open(target,"a",newline="")as file:
+        writer=csv.writer(file)
+        writer.writerow([
+            movement.date,
+            movement.title,
+            movement.category.name,
+            movement.movement_type,
+            movement.amount
+        ])
+
+def initialize_csv(target=file_name):
+    if os.path.exists(file_name):
+        return
+    with open(file_name,"w",newline="")as file:
+        writer=csv.writer(file)
+        writer.writerow(["Date","Title","Category","Type","Amount"])
 
 def export_to_csv(movements,totals):
     with open(file_name,"w",newline="")as file:
@@ -29,8 +48,13 @@ def load_from_csv():
                     continue
                 if row["Date"].startswith("TOTAL"):
                     break
+                date_str=row["Date"]
+                try:
+                    date=datetime.strptime(date_str,"%d/%m/%Y")
+                except ValueError:
+                    date=datetime.fromisoformat(date_str)
                 movements.append({
-                    "date":datetime.strptime(row["Date"],date_format),
+                    "date":date,
                     "title":row["Title"].strip().capitalize(),
                     "category":row["Category"].strip().capitalize(),
                     "type":row["Type"].strip().capitalize(),
@@ -39,3 +63,4 @@ def load_from_csv():
     except FileNotFoundError:
         pass
     return movements
+
