3499a5ed84f2903a7aa864ee7718f7a484d7f4bc
David Zelaya <dzelayasegura@gmail.com>
2026-03-15 03:25:55 -0600

    Update main.py

--- Semana 8/personal_finance_tracker/main.py
+++ Semana 8/personal_finance_tracker/main.py
@@ -1,14 +1,13 @@
 from interfaces import FinanceInterface
-from persistence import initialize_csv
 from logic import FinanceTracker
 
 
 def main():
-    initialize_csv()
     tracker=FinanceTracker()
     app=FinanceInterface(tracker)
     app.run()
 
 if __name__=="__main__":
     main()
+
 
