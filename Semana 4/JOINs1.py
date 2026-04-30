5e631a85f28a2262c7a5a348a70a21cf2d41f1c8
David Zelaya <dzelayasegura@gmail.com>
2026-04-29 22:25:10 -0600

    Update JOINs1.py

--- Semana 4/JOINs1.py
+++ Semana 4/JOINs1.py
@@ -1,6 +1,7 @@
 All={1,2,3,4,5,6,7,8,9,10}
 Even={2,4,6,8,10}
 Odd={1,3,5,7,9}
+result=All-Odd
 
 union_result=Even | Odd
 print("1. Even u Odd:", union_result)
@@ -9,7 +10,7 @@ intersection_result=Even & Odd
 print("2. Even n Odd", intersection_result)
 
 difference_result=All | Odd
-print("3. All - Odd:", difference_result)
+print("3. All - Odd:", result)
 
 complement_even=All - Even
 print("4. C(Even):", complement_even)
