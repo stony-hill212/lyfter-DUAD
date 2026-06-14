35e276e51d2e43dca727eb3732ce4f27a70bb8d9
David Zelaya <dzelayasegura@gmail.com>
2026-06-14 16:15:01 -0600

    Update __init__.py

--- jwt_authentication/models/__init__.py
+++ jwt_authentication/models/__init__.py
@@ -2,3 +2,7 @@ from .user import User
 from .fruit import Fruit
 from .invoice import Invoice
 from .invoice_detail import InvoiceDetail
+from .login_history import LoginHistory
+from .contact import Contact
+from .purchase_m import Purchase
+from .purchaseItems import PurchaseItem
