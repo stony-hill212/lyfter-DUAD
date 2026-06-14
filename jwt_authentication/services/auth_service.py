567b835c2b539d02149d833b18a2d3233be14ddf
David Zelaya <dzelayasegura@gmail.com>
2026-06-14 16:42:13 -0600

    Update auth_service.py

--- jwt_authentication/services/auth_service.py
+++ jwt_authentication/services/auth_service.py
@@ -1,4 +1,5 @@
 from repositories.user_repository import UserRepository
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class AuthService:
     @staticmethod
@@ -6,16 +7,17 @@ class AuthService:
         existing_user=(UserRepository.get_by_username(username))
         if existing_user:
             return None
+        hashed_password=generate_password_hash(password)
         return UserRepository.create_user(
             username,
-            password
+            hashed_password
         )
     @staticmethod
     def login(username, password):
         user=UserRepository.get_by_username(username)
         if user is None:
             return None
-        if user.password!=password:
+        if not check_password_hash(user.password, password):
             return None
         return user
     
