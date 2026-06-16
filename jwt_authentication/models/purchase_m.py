17e23921666ee872494318a4b462ec45f3537231
David Zelaya <dzelayasegura@gmail.com>
2026-06-16 04:05:50 -0600

    Delete jwt_authentication/models/purchase_m.py

--- jwt_authentication/models/purchase_m.py
+++
@@ -1,8 +0,0 @@
-from extensions import db
-from datetime import datetime
-
-class Purchase(db.Model):
-    __tablename__="purchases"
-    id=db.Column(db.Integer, primary_key=True)
-    created_at=db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    items=db.relationship("PurchaseItem", backref="purchase", lazy=True, cascade="all, delete-orphan")
