49fc96ea271ba08f174aa798b790b429100f04cf
David Zelaya <dzelayasegura@gmail.com>
2026-06-16 04:10:53 -0600

    Update purchase_service.py

--- jwt_authentication/services/purchase_service.py
+++ jwt_authentication/services/purchase_service.py
@@ -1,16 +1,16 @@
 from extensions import db
-from models.purchase_m import Purchase
-from models.purchaseItems import PurchaseItem
-from repositories.purchase_repo import PurchaseRepo
 from repositories.fruit_repo import FruitRepository
+from repositories.invoice_repository import InvoiceRepository
 
 class PurchaseService:
     @staticmethod
-    def create_purchase(items):
+    def create_purchase(user_id, items):
         try:
-            purchase=Purchase()
-            PurchaseRepo.create_purchase(purchase)
-            db.session.flush()
+            invoice=InvoiceRepository.create_invoice(
+                user_id=user_id,
+                total=0
+            )
+            total=0
             for item_data in items:
                 fruit=FruitRepository.get_by_id(item_data["fruit_id"])
                 if not fruit:
@@ -20,15 +20,19 @@ class PurchaseService:
                     raise ValueError("Quantity must be greater than 0")
                 if fruit.amount<quantity:
                     raise ValueError(f"Not enough stock for {fruit.name}")
+                subtotal=fruit.price*quantity
+                total+=subtotal
                 fruit.amount-=quantity
-                purchase_item=PurchaseItem(
-                    purchase_id=purchase.id,
-                    fruit_id=fruit.id,
-                    quantity=quantity
+                InvoiceRepository.create_invoice_details(
+                    invoice.id,
+                    fruit.id,
+                    quantity,
+                    fruit.price,
+                    subtotal
                 )
-                PurchaseRepo.create_item(purchase_item)
-            PurchaseRepo.commit()
-            return purchase
+            invoice.total=total
+            InvoiceRepository.commit()
+            return invoice
         except Exception:
-            PurchaseRepo.rollback()
+            InvoiceRepository.rollback()
             raise
