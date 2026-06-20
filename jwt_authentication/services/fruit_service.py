b05c09d007f48dafc25acecb0658672b72546722
David Zelaya <dzelayasegura@gmail.com>
2026-06-20 05:43:47 -0600

    Update fruit_service.py

--- jwt_authentication/services/fruit_service.py
+++ jwt_authentication/services/fruit_service.py
@@ -1,23 +1,47 @@
 from repositories.fruit_repo import FruitRepository
 from datetime import datetime
+from extensions import redis_client
+import json
 
 class FruitService:
     @staticmethod
     def create_fruit(data):
-        return FruitRepository.create_fruit(
+        fruit=FruitRepository.create_fruit(
             data["name"],
             data["price"],
             data["amount"],
             datetime.strptime(data["arrival_date"],"%Y-%m-%d").date()
         )
+        redis_client.delete("fruits:all")
+        return fruit
 
     @staticmethod
     def get_all_fruits():
-        return FruitRepository.get_all()
+        cache_key="fruits:all"
+        cached_fruits=redis_client.get(cache_key)
+        if cached_fruits:
+            print("ALL FRUITS HIT THE CACHE")
+            return json.loads(cached_fruits)
+        print("ALL FRUITS CACHE MISS")
+        fruits=FruitRepository.get_all()
+        fruits_data=[fruit.to_dict() for fruit in fruits]
+        redis_client.set(cache_key, json.dumps(fruits_data, default=str))
+        return fruits_data
     
     @staticmethod
     def get_fruit_by_id(fruit_id):
-        return FruitRepository.get_by_id(fruit_id)
+        cache_key=f"fruit: {fruit_id}"
+        cached_fruit=redis_client.get(cache_key)
+        if cached_fruit:
+            print("CACHE HIT")
+            return json.loads(cached_fruit)
+        print("CACHE MISS")
+        fruit=FruitRepository.get_by_id(fruit_id)
+        if not fruit:
+            return None
+        fruit_data=fruit.to_dict()
+        redis_client.set(cache_key, json.dumps(fruit_data, default=str))
+        return fruit_data
     
     @staticmethod
     def update_fruit(fruit_id, data):
@@ -29,6 +53,8 @@ class FruitService:
         fruit.amount=data.get("amount", fruit.amount)
         fruit.arrival_date=data.get("arrival_date", fruit.arrival_date)
         FruitRepository.update()
+        redis_client.delete(f"fruit: {fruit_id}")
+        redis_client.delete("fruits:all")
         return fruit
     
     @staticmethod
@@ -37,4 +63,6 @@ class FruitService:
         if not fruit:
             return False
         FruitRepository.delete(fruit)
+        redis_client.delete(f"fruit: {fruit_id}")
+        redis_client.delete("fruits:all")
         return True
