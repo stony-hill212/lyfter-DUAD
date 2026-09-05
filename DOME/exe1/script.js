5be29165cb8d02c2a8fefe6f49536e83f6f0cce7
David Zelaya <dzelayasegura@gmail.com>
2026-09-05 03:25:52 -0600

    Update script.js

--- DOME/exe1/script.js
+++ DOME/exe1/script.js
@@ -1,6 +1,15 @@
-const enochText= document.getElementById("enochText");
+const colors= [
+    "#7b1113",
+    "#1f4ef5",
+    "#55ea3b",
+    "#e57711",
+    "#3b6e4f"
+]
+
 const colorButton= document.getElementById("colorButton");
+const enochText= document.getElementById("enochText");
 
 colorButton.addEventListener("click", function () {
-    enochText.classList.toggle("revealed");
+    const randomColor= colors[Math.floor(Math.random()*colors.length)];
+    enochText.style.color= randomColor;
 });
