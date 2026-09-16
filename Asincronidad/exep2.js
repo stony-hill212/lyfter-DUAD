a04ac9c8d8293914b92f52e1292d6ea961117bd0
David Zelaya <dzelayasegura@gmail.com>
2026-09-16 04:50:40 -0600

    Update exep2.js

--- Asincronidad/exep2.js
+++ Asincronidad/exep2.js
@@ -1,10 +1,23 @@
-const pokemon1= fetch("https://pokeapi.co/api/v2/pokemon/25");
-const pokemon2= fetch("https://pokeapi.co/api/v2/pokemon/6");
-const pokemon3= fetch("https://pokeapi.co/api/v2/pokemon/150");
-
-Promise.any([pokemon1, pokemon2, pokemon3]).then(response=> {
+const pokemon1= fetch("https://pokeapi.co/api/v2/pokemon/25").then(response=> {
+    if(!response.ok) {
+        throw new Error("Pokemon 1 not found");
+    }
     return response.json();
-}).then(pokemon=> {
+});
+const pokemon2= fetch("https://pokeapi.co/api/v2/pokemon/6").then(response=> {
+    if(!response.ok) {
+        throw new Error("Pokemon 2 not found");
+    }
+    return response.json();
+});
+const pokemon3= fetch("https://pokeapi.co/api/v2/pokemon/150").then(response=> {
+    if(!response.ok) {
+        throw new Error("Pokemon 3 not found");
+    }
+    return response.json();
+});
+
+Promise.any([pokemon1, pokemon2, pokemon3]).then(pokemon=> {
     console.log(`The first Pokemon resolved is: ${pokemon.name}`);
 }).catch(error=> {
     if (error instanceof AggregateError) {
