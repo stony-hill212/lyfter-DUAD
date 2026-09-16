23b633acfeee09a5ec2339302cbca77d5cecb284
David Zelaya <dzelayasegura@gmail.com>
2026-09-16 02:17:34 -0600

    Update exep1.js

--- Asincronidad/exep1.js
+++ Asincronidad/exep1.js
@@ -1,13 +1,10 @@
-const pokemon1= fetch("https://pokeapi.co/api/v2/pokemon/25");
-const pokemon2= fetch("https://pokeapi.co/api/v2/pokemon/6");
-const pokemon3= fetch("https://pokeapi.co/api/v2/pokemon/150");
+const pokemon1= fetch("https://pokeapi.co/api/v2/pokemon/25").then(response=>response.json());
+const pokemon2= fetch("https://pokeapi.co/api/v2/pokemon/6").then(response=>response.json());
+const pokemon3= fetch("https://pokeapi.co/api/v2/pokemon/150").then(response=>response.json());
 
-Promise.all([pokemon1, pokemon2, pokemon3]).then(responses=> {
-    return Promise.all(
-        responses.map(response=> response.json())
-    );
-}).then(pokemon=> {
+Promise.all([pokemon1, pokemon2, pokemon3]).then(pokemon=> {
     console.log(pokemon);
-}).catch(error=> {
+})
+.catch(error=> {
     console.log("Something went wrong:", error);
 });
