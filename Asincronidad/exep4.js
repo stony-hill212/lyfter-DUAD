1fe0f19d6c433c9389850774f399972494f3060f
David Zelaya <dzelayasegura@gmail.com>
2026-09-16 02:50:35 -0600

    Update exep4.js

--- Asincronidad/exep4.js
+++ Asincronidad/exep4.js
@@ -1,4 +1,4 @@
-fetch("https://reqres.in/api/users/23").then(response=> {
+fetch("https://reqres.in/api/users/2").then(response=> {
     if (!response.ok) {throw new Error("User not found");}
     return response.json();
 }).then(user=> {
